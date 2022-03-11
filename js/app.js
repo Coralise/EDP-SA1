7518fd2b1fb8beea342cfa0efd446015d6223269
Corals <waynegabule@gmail.com>
2022-03-12 05:20:22 +0800

    Initial Setup

--- js/app.js
+++ js/app.js
@@ -1,13 +1,2 @@
 var msg = 'Hello World';
 console.log(msg);
-
-const app = require("express")();
-const PORT = process.env.PORT || 3000;
-
-app.get("", (req, res) => {
-    res.send("Hello, world!!");
-});
-
-app.listen(PORT, () => {
-    console.log(`App up at port ${PORT}`);
-})
